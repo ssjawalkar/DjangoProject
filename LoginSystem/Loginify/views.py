cb570dce864cdfa178e3cf76136fd5c1ec9e7321
samihan <samihan.jawalkarusa@users.noreply.github.com>
2024-12-08 12:04:24 -0600

    CREATE VIEWS AND URLS FOR LOGIN SYSTEM

--- LoginSystem/Loginify/views.py
+++ LoginSystem/Loginify/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import UserDetail
+from django.http import HttpResponse
 # Create your views here.
+def signup_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        if not UserDetail.objects.filter(email=email).exists():
+            UserDetail.objects.create(username=username,email=email,password=password)
+            return redirect('login')
+    return render(request,"signup.html")
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        userValid = UserDetail.objects.filter(email=email, password=password).first()
+        if userValid:
+            return HttpResponse("Login Succeeful!!!")
+    return render(request,"login.html")
